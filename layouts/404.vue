70439bb7f81722336d0c40f680b4214519ef8237
ghostrider-05 <52837878+ghostrider-05@users.noreply.github.com>
2021-06-29 16:48:54 +0200

    theme layouts

--- layouts/404.vue
+++ layouts/404.vue
@@ -1,30 +1,135 @@
 <template>
-  <div class="theme-container">
-    <div class="theme-default-content">
-      <h1>404</h1>
+  <div v-bind:style="getBackground()">
+    <div class="theme-container">
+      <div class="content">
+        <h1>{{ title }}</h1>
+        <blockquote>{{ getMsg() }}</blockquote>
 
-      <blockquote>{{ getMsg() }}</blockquote>
+        <router-link v-bind:to="site.base">
+          {{ theme.backToHome }}
+        </router-link>
 
-      <RouterLink to="/">
-        Take me home.
-      </RouterLink>
+        <br /><br />
+
+        <div>
+          <div v-if="enabledSuggestedPages()">
+            <p>{{ suggestionTitle }}</p>
+            <li v-for="page in getRecommendedPages()" :key="page.url">
+              <a :href="page.link">{{ page.text }}</a>
+            </li>
+          </div>
+
+          <div v-if="enabledReport()">
+            <p>
+              {{ getReportText()[0] }}
+              <a :href="githubIssuesUrl()">
+                {{ getReportText()[1] }}
+              </a>
+            </p>
+          </div>
+        </div>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
-const msgs = [
-  `There's nothing here.`,
-  `How did we get here?`,
-  `That's a Four-Oh-Four.`,
-  `Looks like we've got some broken links.`
-]
-
 export default {
+  data() {
+    return {
+      theme: this.$site.themeConfig,
+      site: this.$site,
+      // local notFound settings
+      notFoundSettings: this.$site.themeConfig.local.notFound,
+      // default titles
+      title: "404",
+      suggestionTitle: "Suggested pages:",
+      // default recommended pages
+      recommendedPages: [
+        {
+          text: "Home",
+          link: "/",
+        },
+      ],
+    };
+  },
+
   methods: {
-    getMsg () {
-      return msgs[Math.floor(Math.random() * msgs.length)]
-    }
+    getMsg() {
+      const { notFound } = this.theme;
+
+      const index = Math.floor(Math.random() * notFound.length);
+      return notFound[index];
+    },
+
+    getBackground() {
+      const backgroundImage = this.theme.local.notFound.background;
+
+      return backgroundImage
+        ? {
+            "background-image": `url(${backgroundImage})`,
+            height: "100vh",
+            "background-position": "center",
+            "background-repeat": "no-repeat",
+            "background-size": "cover",
+            display: "flow-root",
+          }
+        : {};
+    },
+
+    getRecommendedPages() {
+      const pages = this.notFoundSettings.suggestedPages;
+
+      return pages || this.recommendedPages;
+    },
+
+    getReportText() {
+      return [
+        "To help improve this guide you can report this broken link in a",
+        "new github issue",
+      ];
+    },
+
+    githubIssuesUrl() {
+      return `https://github.com/${this.theme.repo}/issues`;
+    },
+
+    enabledReport() {
+      const isReportEnabled = this.notFoundSettings.report;
+
+      return this.theme.repo && (isReportEnabled || true);
+    },
+
+    enabledSuggestedPages() {
+      const showSuggestedPages = this.notFoundSettings.showSuggestedPages;
+
+      return showSuggestedPages || true;
+    },
+  },
+};
+</script>
+
+
+<style scoped >
+html,
+body {
+  height: 100%;
+  margin: 0px;
+  padding: 0px;
+}
+
+.content {
+  max-width: 740px;
+  margin: 0 auto;
+}
+
+.content > h1 {
+  padding-top: 4.6rem;
+}
+
+@media screen and (max-width: 959px) {
+  .content {
+    padding: 2rem;
   }
 }
-</script>
+</style>
---
+++ layouts/Home.vue
@@ -0,0 +1,247 @@
+<template>
+  <Layout>
+    <template #page-header>
+      <div v-if="fm && (fm.hero || fm.events)">
+        <div
+          v-if="fm.hero"
+          class="home-hero"
+          :style="
+            heroProp('background')
+              ? {
+                  'background-image': `linear-gradient(180deg, rgba(0, 134, 29, 0.33) 0%, #000000 100%), url(${heroProp(
+                    'background'
+                  )})`,
+                  'background-position': 'center',
+                  'background-repeat': 'no-repeat',
+                  'background-size': 'cover',
+                  display: 'flow-root',
+                }
+              : {}
+          "
+        >
+          <h1>{{ heroProp("title") }}</h1>
+          <p>{{ heroProp("description") }}</p>
+          <a
+            v-for="(action, i) in heroProp('actions')"
+            :key="i.toString()"
+            class="hero-actions"
+            :href="Object.values(action)[0][1]"
+          >
+            <button :class="'primary' in action ? 'primary' : 'secondary'">
+              {{ Object.values(action)[0][0] }}
+            </button>
+          </a>
+        </div>
+
+        <div v-if="fm.events" class="home-events">
+          <div v-for="(event, index) in fm.events" :key="index" class="event">
+            <div class="event-text">
+              <h1>{{ event.title }}</h1>
+              <div>
+                <p>{{ event.description }}</p>
+              </div>
+              <a
+                :href="action.link"
+                v-for="(action, i) in event.actions"
+                :key="i"
+              >
+                <button
+                  :class="{
+                    primary: action.primary,
+                    'secondary border': !action.primary,
+                    margin: i !== 0,
+                  }"
+                  class="event-buttons"
+                >
+                  {{ action.text }}
+                </button>
+              </a>
+            </div>
+
+            <div class="event-image">
+              <img :src="event.image" :alt="event.title" />
+            </div>
+          </div>
+        </div>
+      </div>
+    </template>
+  </Layout>
+</template>
+
+<script>
+import Layout from "@theme/layouts/Layout.vue";
+
+export default {
+  components: {
+    Layout,
+  },
+
+  data() {
+    return {
+      fm: "",
+    };
+  },
+
+  methods: {
+    heroProp(prop) {
+      if (this.fm.hero.some((x) => x[prop])) {
+        return this.fm.hero.find((x) => x[prop])[prop];
+      } else return ''
+    },
+  },
+
+  mounted() {
+    this.fm = this.$page.frontmatter;
+    document.getElementsByClassName("last-updated")[0].style.display = "none";
+  },
+};
+</script>
+
+<style scoped>
+body {
+  --accent: #3eaf7c;
+  --border: #eaecef;
+  --bg: #fff;
+  --text: #2c3e50;
+}
+
+button {
+  padding: 10px 18px;
+  margin: 0px 12px;
+  border: none;
+  border-radius: 4px;
+}
+
+button:hover {
+  cursor: pointer;
+}
+
+button.primary {
+  background-color: #3eaf7c !important;
+  color: white;
+}
+
+button.secondary a {
+  background-color: var(--bg);
+  color: var(--text);
+}
+
+button.border {
+  border: 2px solid var(--border);
+}
+
+.event-buttons:not(:first-of-type) {
+  margin-left: 10px !important;
+}
+
+.event-buttons.margin {
+  margin-left: 10px;
+}
+
+.home-hero {
+  height: calc(100vh - 3.6rem);
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  padding-left: 10vw;
+  max-width: 100vw !important;
+  margin-top: 3.6rem;
+}
+
+.home-hero h1 {
+  text-transform: uppercase;
+  color: white;
+  font-size: 84px;
+  font-weight: bold;
+  line-height: 5rem;
+  transform: translateY(30px);
+  margin-right: 20px;
+  max-width: 850px;
+}
+.home-hero p {
+  color: white;
+  max-width: 600px;
+  padding-bottom: 12px;
+  padding-right: 5vw;
+  font-size: 30px;
+}
+
+.home-events {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+}
+
+.event {
+  padding-top: 50px;
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+}
+
+.event-text div {
+  margin-left: 10vw;
+  max-width: 500px;
+  padding-right: 5vw;
+}
+
+.event-text button {
+  margin-left: 10vw;
+}
+
+.event-image {
+  display: flex;
+  flex-direction: row;
+  width: 50%;
+  padding-left: 15px;
+  padding-right: 5vw;
+}
+
+.event-image img {
+  max-width: 600px !important;
+}
+
+.event-text h1 {
+  text-transform: uppercase;
+  font-size: 30px;
+  margin-left: 10vw;
+  margin-bottom: 24px;
+}
+
+.event-text h1 span {
+  color: var(--accent);
+  font-size: 26px;
+}
+
+@media screen and (max-width: 600px) {
+  .event-image,
+  .event-text h1 {
+    padding-left: 5vw;
+  }
+  .event {
+    flex-direction: column;
+  }
+  .event-image img {
+    max-width: 90vw !important;
+  }
+  .home-hero h1 {
+    font-size: 48px;
+  }
+}
+
+@media screen and (max-width: 1000px) {
+  .event-image {
+    width: 90vw;
+  }
+}
+
+@media screen and (max-width: 1215px) {
+  .event {
+    flex-direction: column;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+}
+</style>
