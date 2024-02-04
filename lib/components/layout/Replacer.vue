2690177e98ecb4ba7f445d93bfe8a9eec336e9e8
ghostrider-05 <lucas.duijn@hotmail.nl>
2024-02-04 17:00:44 +0100

    home: move teleport to component

---
+++ lib/components/layout/Replacer.vue
@@ -0,0 +1,20 @@
+<template>
+    <ClientOnly>
+        <Teleport :to="to">
+            <slot />
+        </Teleport>
+    </ClientOnly>
+</template>
+
+<script setup lang="ts">
+import { onMounted } from 'vue';
+
+const { to } = defineProps<{
+    to: string
+}>()
+
+onMounted(() => {
+    document.styleSheets.item(0)?.insertRule(`${to} * { display: none }`)
+    document.styleSheets.item(0)?.insertRule(`${to} *:last-child { display: flex }`)
+})
+</script>
--- lib/layouts/Layout/components/main.vue
+++ lib/layouts/Layout/components/main.vue
@@ -2,11 +2,9 @@
     <ParentLayout :class="{ 'banner-dismissed': !(banner != undefined && banner !== false) }">
         <template #layout-top>
             <Banner v-bind="banner" v-if="banner != undefined && banner !== false" />
-            <ClientOnly>
-                <Teleport to=".VPHomeSponsors .love">
-                    <Icon :icon="theme.home?.sponsorLogo ?? 'fa-brands:patreon'" class="icon" fill="currentColor" />
-                </Teleport>
-            </ClientOnly>
+            <Replacer to=".VPHomeSponsors .love">
+                <Icon :icon="theme.home?.sponsorLogo ?? 'fa-brands:patreon'" class="icon" width="28px" fill="currentColor" />
+            </Replacer>
         </template>
 
         <template v-for="homeSlot in Object.keys(homepageSlots ?? {}).filter(s => homepageSlots![s])" #[homeSlot]>
@@ -45,8 +43,6 @@ import { useCssVar } from '@vueuse/core'
 import { useData, useRouter } from 'vitepress'
 import DefaultTheme from 'vitepress/theme'
 
-// import markdownit from 'markdown-it'
-
 import { useMarkdown, useStorage } from '../../../composables'
 import type {
     BannerNotification,
@@ -58,6 +54,7 @@ import Banner from '../../../components/layout/Banner.vue'
 import Feedback from '../../../components/layout/Feedback.vue'
 import NavInbox from '../../../components/layout/Inbox.vue'
 import RelatedPages from '../../../components/layout/RelatedPages.vue'
+import Replacer from '../../../components/layout/Replacer.vue'
 
 import { renderSidebarAction } from '../sections/sidebarAction'
 import { renderNotFinished } from '../sections/notFinished'
