db2bc42d5dfe9bc84d090a8c433f817f9d1a01b2
ghostrider-05 <lucas.duijn@hotmail.nl>
2026-03-09 17:00:26 +0100

    feat(Tabs): add time switch and style options

--- lib/components/global/markdown/tabs.vue
+++ lib/components/global/markdown/tabs.vue
@@ -6,7 +6,7 @@
                 {{ name }}
             </p>
         </div>
-        <hr class="tabs-divider">
+        <hr class="tabs-divider" v-if="!hideDivider">
         <div v-for="name, index in getNames()" :key="name">
             <slot :name="toSlug(name)" v-if="index === active" />
         </div>
@@ -19,6 +19,7 @@ import { useStorage } from '../../../composables/';
 import { useUrlSearchParams } from '@vueuse/core';
 
 const active = ref(0);
+const timer = ref<NodeJS.Timeout>()
 const props = withDefaults(defineProps<{
     tabs: string[],
     startTab?: string,
@@ -27,23 +28,29 @@ const props = withDefaults(defineProps<{
         hideSelectorOnSet?: boolean
     },
     hideSelector?: boolean,
+    hideDivider?: boolean
     searchParam?: string | null,
     updateSearchParam?: boolean,
     tabStyle?: object,
     activeTabStyle?: object,
     alignLeft?: boolean
+    alignCenter?: boolean
+    switchTimeout?: number
 }>(), {
     hideSelector: false,
+    hideDivider: false,
     alignLeft: false,
+    alignCenter: false,
     searchParam: null,
     updateSearchParam: true,
+    switchTimeout: -1,
     tabStyle: () => ({}),
     activeTabStyle: () => ({}),
 })
 
 const params = useUrlSearchParams()
 const hideSelectorActions = ref(false)
-const justifyTabContent = props.alignLeft ? 'space-start' : 'space-between'
+const justifyTabContent = props.alignLeft ? 'space-start' : props.alignCenter ? 'center' : 'space-between'
 const textMargin = props.alignLeft ? '10px 0 0 0 !important' : '10px auto 0 auto !important'
 
 function toSlug(name: string): string {
@@ -56,9 +63,9 @@ function getNames(): string[] {
     })
 }
 
-function changeActive(tabName: string): void;
-function changeActive(index: number): void;
-function changeActive(index: number | string): void {
+function changeActive(tabName: string, resetTimer?: boolean): void;
+function changeActive(index: number, resetTimer?: boolean): void;
+function changeActive(index: number | string, resetTimer = true): void {
     const value = typeof index === 'string'
         ? Math.max(props.tabs.indexOf(index), 0)
         : index
@@ -68,18 +75,32 @@ function changeActive(index: number | string): void {
     if (props.searchParam != null && props.updateSearchParam) {
         params[props.searchParam] = toSlug(props.tabs[value]).slice(4)
     }
+
+    // Reset switch timer
+    if (props.switchTimeout > 0 && resetTimer) {
+        clearInterval(timer.value)
+        timer.value = setInterval(changeActiveToNext, props.switchTimeout)
+    }
+}
+
+function changeActiveToNext (): void {
+    const currentIndex = active.value
+    const nextIndex = currentIndex + 1 >= props.tabs.length ? 0 : (currentIndex + 1)
+
+    return changeActive(nextIndex)
 }
 
 onMounted(() => {
     const { hideSelector, searchParam, startTab, storage } = props
 
     hideSelectorActions.value = hideSelector
-    if (startTab != undefined) changeActive(startTab)
+    if (startTab != undefined) changeActive(startTab, false)
 
     if (storage != undefined) {
         const stored = useStorage().get(<never>props.storage?.key)
         if (stored != null) {
-            changeActive(stored)
+            changeActive(stored, false)
+
             if (storage.hideSelectorOnSet != undefined) {
                 hideSelectorActions.value = storage.hideSelectorOnSet
             }
@@ -89,8 +110,12 @@ onMounted(() => {
     if (searchParam != null) {
         const tabParam = params[searchParam]?.toString()
         if (tabParam && tabParam.length && props.tabs.map(toSlug).includes(toSlug(tabParam))) {
-            changeActive(tabParam)
+            changeActive(tabParam, false)
         }
+    }
+
+    if (props.switchTimeout > 0) {
+        timer.value = setInterval(changeActiveToNext, props.switchTimeout)
     }
 })
 </script>
